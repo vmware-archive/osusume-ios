9e8cbbfd6ccf7018d51f9272b6447d05080f44e8
Yuki Nishijima <ynishijima@pivotal.io>
2015-12-02 15:37:04 +0900

    Initial Commit

---
+++ Osusume/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Osusume
+//
+//  Created by pivotal on 12/2/15.
+//  Copyright © 2015 io.pivotal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
