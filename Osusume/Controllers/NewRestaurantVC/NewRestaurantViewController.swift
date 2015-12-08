e334d537128abc7f336e7d450393774e92f6696d
Yuki Nishijima <ynishijima@pivotal.io>
2015-12-08 10:51:49 +0900

    Add text fields for address, cuisine type, dish name
    [Finishes #109335334]
    
    Signed-off-by: Ichizo Umehara <iumehara@pivotal.io>

--- Osusume/Controllers/NewRestaurantVC/NewRestaurantViewController.swift
+++ Osusume/Controllers/NewRestaurantVC/NewRestaurantViewController.swift
@@ -11,7 +11,43 @@ class NewRestaurantViewController : UIViewController {
 
     let restaurantNameLabel : UILabel = {
         let label = UILabel()
-        label.text = "restaurant name"
+        label.text = "Restaurant Name"
+        return label
+    }()
+
+    let restaurantAddressTextField : UITextField = {
+        let textField = UITextField.newAutoLayoutView()
+        textField.borderStyle = .Line
+        return textField
+    }()
+
+    let restaurantAddressLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Address"
+        return label
+    }()
+
+    let restaurantCuisineTypeTextField : UITextField = {
+        let textField = UITextField.newAutoLayoutView()
+        textField.borderStyle = .Line
+        return textField
+    }()
+
+    let restaurantCuisineTypeLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Cuisine Type"
+        return label
+    }()
+
+    let restaurantDishNameTextField : UITextField = {
+        let textField = UITextField.newAutoLayoutView()
+        textField.borderStyle = .Line
+        return textField
+    }()
+
+    let restaurantDishNameLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Name of a Dish"
         return label
     }()
 
@@ -30,7 +66,13 @@ class NewRestaurantViewController : UIViewController {
         view.addSubview(backgroundImage)
         view.addSubview(restaurantNameTextField)
         view.addSubview(restaurantNameLabel)
-        
+        view.addSubview(restaurantAddressTextField)
+        view.addSubview(restaurantAddressLabel)
+        view.addSubview(restaurantCuisineTypeTextField)
+        view.addSubview(restaurantCuisineTypeLabel)
+        view.addSubview(restaurantDishNameTextField)
+        view.addSubview(restaurantDishNameLabel)
+
         view.setNeedsUpdateConstraints()
     }
     
@@ -46,6 +88,30 @@ class NewRestaurantViewController : UIViewController {
             restaurantNameTextField.autoPinEdge(.Trailing, toEdge: .Trailing, ofView: restaurantNameLabel)
             restaurantNameTextField.autoPinEdge(.Top, toEdge: .Bottom, ofView: restaurantNameLabel)
             
+            restaurantAddressLabel.autoPinEdge(.Leading, toEdge: .Leading, ofView: restaurantNameTextField)
+            restaurantAddressLabel.autoPinEdge(.Trailing, toEdge: .Trailing, ofView: restaurantNameTextField)
+            restaurantAddressLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: restaurantNameTextField)
+
+            restaurantAddressTextField.autoPinEdge(.Leading, toEdge: .Leading, ofView: restaurantAddressLabel)
+            restaurantAddressTextField.autoPinEdge(.Trailing, toEdge: .Trailing, ofView: restaurantAddressLabel)
+            restaurantAddressTextField.autoPinEdge(.Top, toEdge: .Bottom, ofView: restaurantAddressLabel)
+
+            restaurantCuisineTypeLabel.autoPinEdge(.Leading, toEdge: .Leading, ofView: restaurantAddressTextField)
+            restaurantCuisineTypeLabel.autoPinEdge(.Trailing, toEdge: .Trailing, ofView: restaurantAddressTextField)
+            restaurantCuisineTypeLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: restaurantAddressTextField)
+
+            restaurantCuisineTypeTextField.autoPinEdge(.Leading, toEdge: .Leading, ofView: restaurantCuisineTypeLabel)
+            restaurantCuisineTypeTextField.autoPinEdge(.Trailing, toEdge: .Trailing, ofView: restaurantCuisineTypeLabel)
+            restaurantCuisineTypeTextField.autoPinEdge(.Top, toEdge: .Bottom, ofView: restaurantCuisineTypeLabel)
+
+            restaurantDishNameLabel.autoPinEdge(.Leading, toEdge: .Leading, ofView: restaurantCuisineTypeTextField)
+            restaurantDishNameLabel.autoPinEdge(.Trailing, toEdge: .Trailing, ofView: restaurantCuisineTypeTextField)
+            restaurantDishNameLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: restaurantCuisineTypeTextField)
+
+            restaurantDishNameTextField.autoPinEdge(.Leading, toEdge: .Leading, ofView: restaurantDishNameLabel)
+            restaurantDishNameTextField.autoPinEdge(.Trailing, toEdge: .Trailing, ofView: restaurantDishNameLabel)
+            restaurantDishNameTextField.autoPinEdge(.Top, toEdge: .Bottom, ofView: restaurantDishNameLabel)
+
             didSetupConstraints = true
         }
         super.updateViewConstraints()
--- OsusumeTests/NewRestaurantViewControllerSpec.swift
+++ OsusumeTests/NewRestaurantViewControllerSpec.swift
@@ -18,7 +18,7 @@ class NewRestaurantViewControllerSpec: QuickSpec {
                 let view = subject.view
 
                 expect(view.subviews.contains(subject.restaurantNameLabel)).to(beTrue())
-                expect(subject.restaurantNameLabel.text).to(equal("restaurant name"))
+                expect(subject.restaurantNameLabel.text).to(equal("Restaurant Name"))
                 expect(subject.restaurantNameLabel.frame).notTo(equal(CGRect.zero))
 
                 expect(view.subviews.contains(subject.restaurantNameTextField)).to(beTrue())
@@ -27,6 +27,51 @@ class NewRestaurantViewControllerSpec: QuickSpec {
 
                 subject.restaurantNameTextField.text = "Some Restaurant"
                 expect(subject.restaurantNameTextField.text).to(equal("Some Restaurant"))
+            }
+
+            it("can enter address") {
+                let view = subject.view
+
+                expect(view.subviews.contains(subject.restaurantAddressLabel)).to(beTrue())
+                expect(subject.restaurantAddressLabel.text).to(equal("Address"))
+                expect(subject.restaurantAddressLabel.frame).notTo(equal(CGRect.zero))
+
+                expect(view.subviews.contains(subject.restaurantAddressTextField)).to(beTrue())
+                expect(subject.restaurantAddressTextField.borderStyle) == UITextBorderStyle.Line
+                expect(subject.restaurantAddressTextField.frame.origin).notTo(equal(CGPoint.zero))
+
+                subject.restaurantAddressTextField.text = "Restaurant address"
+                expect(subject.restaurantAddressTextField.text).to(equal("Restaurant address"))
+            }
+
+            it("can enter cuisine type") {
+                let view = subject.view
+
+                expect(view.subviews.contains(subject.restaurantCuisineTypeLabel)).to(beTrue())
+                expect(subject.restaurantCuisineTypeLabel.text).to(equal("Cuisine Type"))
+                expect(subject.restaurantCuisineTypeLabel.frame).notTo(equal(CGRect.zero))
+
+                expect(view.subviews.contains(subject.restaurantCuisineTypeTextField)).to(beTrue())
+                expect(subject.restaurantCuisineTypeTextField.borderStyle) == UITextBorderStyle.Line
+                expect(subject.restaurantCuisineTypeTextField.frame.origin).notTo(equal(CGPoint.zero))
+
+                subject.restaurantCuisineTypeTextField.text = "Restaurant Cuisine Type"
+                expect(subject.restaurantCuisineTypeTextField.text).to(equal("Restaurant Cuisine Type"))
+            }
+
+            it("can enter name of a dish") {
+                let view = subject.view
+
+                expect(view.subviews.contains(subject.restaurantDishNameLabel)).to(beTrue())
+                expect(subject.restaurantDishNameLabel.text).to(equal("Name of a Dish"))
+                expect(subject.restaurantDishNameLabel.frame).notTo(equal(CGRect.zero))
+
+                expect(view.subviews.contains(subject.restaurantDishNameTextField)).to(beTrue())
+                expect(subject.restaurantDishNameTextField.borderStyle) == UITextBorderStyle.Line
+                expect(subject.restaurantDishNameTextField.frame.origin).notTo(equal(CGPoint.zero))
+
+                subject.restaurantDishNameTextField.text = "Sushi"
+                expect(subject.restaurantDishNameTextField.text).to(equal("Sushi"))
             }
         }
     }
