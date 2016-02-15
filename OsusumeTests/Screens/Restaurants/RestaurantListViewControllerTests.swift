30e3ca5b6f898082660054fcbcd2a36141245257
Derek Boire <dboire@pivotal.io>
2016-02-15 11:24:41 +0900

    Remove duplicate code from RestaurantListVC Tests.
    Signed-off-by: Kyle Gearhart <kgearhart@pivotal.io>

--- OsusumeTests/Screens/Restaurants/RestaurantListViewControllerTests.swift
+++ OsusumeTests/Screens/Restaurants/RestaurantListViewControllerTests.swift
@@ -5,39 +5,58 @@ import Result
 @testable import Osusume
 
 class RestaurantListViewControllerTests: XCTestCase {
-        
+
+    var restaurantListVC: RestaurantListViewController!
+    var router: FakeRouter!
+    var repo: FakeRestaurantRepo!
+
     override func setUp() {
         super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
-    
-    func testRestaurantListVCDisplaysListOfRestaurants() {
-
-        var subject: RestaurantListViewController!
-        var router: FakeRouter!
-        var repo: FakeRestaurantRepo!
 
         UIView.setAnimationsEnabled(false)
         router = FakeRouter()
         repo = FakeRestaurantRepo()
 
         repo.allRestaurants = [
-            Restaurant(id: 1, name: "つけめんTETSU", address: "", cuisineType: "つけめん", offersEnglishMenu: true, walkInsOk: true, acceptsCreditCards: true, notes: "This place is great", author: "Simon", createdAt: NSDate(timeIntervalSince1970: 1454480320), photoUrl: NSURL(string: "")),
-            Restaurant(id: 2, name: "とんかつ 豚組食堂"),
-            Restaurant(id: 3, name: "Coco Curry"),
+            Restaurant(
+                id: 1,
+                name: "つけめんTETSU",
+                address: "",
+                cuisineType: "つけめん",
+                offersEnglishMenu: true,
+                walkInsOk: true,
+                acceptsCreditCards: true,
+                notes: "This place is great",
+                author: "Simon",
+                createdAt: NSDate(timeIntervalSince1970: 1454480320),
+                photoUrl: NSURL(string: "")
+            ),
+            Restaurant(
+                id: 2,
+                name: "とんかつ 豚組食堂"
+            ),
+            Restaurant(
+                id: 3,
+                name: "Coco Curry"
+            ),
         ]
-        subject = RestaurantListViewController(router: router, repo: repo)
-        subject.view.layoutSubviews()
+        restaurantListVC = RestaurantListViewController(router: router, repo: repo)
+        restaurantListVC.view.layoutSubviews()
+    }
 
-        let tableView = subject.tableView
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testRestaurantListVCDisplaysListOfRestaurants() {
+        let tableView = restaurantListVC.tableView
 
         expect(tableView.numberOfSections).to(equal(1))
         expect(tableView.numberOfRowsInSection(0)).to(equal(3))
 
-        let firstTableViewCell : RestaurantTableViewCell = tableView.cellForRowAtIndexPath(NSIndexPath(forItem: 0, inSection: 0)) as! RestaurantTableViewCell
+        let firstTableViewCell : RestaurantTableViewCell = tableView.cellForRowAtIndexPath(
+            NSIndexPath(forItem: 0, inSection: 0)
+        ) as! RestaurantTableViewCell
         expect(firstTableViewCell.nameLabel.text).to(equal("つけめんTETSU"))
         expect(firstTableViewCell.cuisineTypeLabel.text).to(equal("つけめん"))
         expect(firstTableViewCell.authorLabel.text).to(equal("Added by Simon"))
@@ -51,49 +70,17 @@ class RestaurantListViewControllerTests: XCTestCase {
     }
 
     func testTapNewRestarauntShowsNewRestaurantScreen() {
+        expect(self.restaurantListVC.navigationItem.rightBarButtonItem).toNot(beNil())
+        expect(self.restaurantListVC.navigationItem.rightBarButtonItem!.title).to(equal("add restaurant"))
 
-        var subject: RestaurantListViewController!
-        var router: FakeRouter!
-        var repo: FakeRestaurantRepo!
+        restaurantListVC.didTapAddRestaurantButton(restaurantListVC.navigationItem.rightBarButtonItem!)
 
-        UIView.setAnimationsEnabled(false)
-        router = FakeRouter()
-        repo = FakeRestaurantRepo()
-
-        repo.allRestaurants = [
-            Restaurant(id: 1, name: "つけめんTETSU", address: "", cuisineType: "つけめん", offersEnglishMenu: true, walkInsOk: true, acceptsCreditCards: true, notes: "This place is great", author: "Simon", createdAt: NSDate(timeIntervalSince1970: 1454480320), photoUrl: NSURL(string: "")),
-            Restaurant(id: 2, name: "とんかつ 豚組食堂"),
-            Restaurant(id: 3, name: "Coco Curry"),
-        ]
-        subject = RestaurantListViewController(router: router, repo: repo)
-        subject.view.layoutSubviews()
-        expect(subject.navigationItem.rightBarButtonItem).toNot(beNil())
-        expect(subject.navigationItem.rightBarButtonItem!.title).to(equal("add restaurant"))
-
-        subject.didTapAddRestaurantButton(subject.navigationItem.rightBarButtonItem!)
-
-        expect(router.newRestaurantScreenIsShowing).to(equal(true))
+        expect(self.router.newRestaurantScreenIsShowing).to(equal(true))
     }
 
     func testSelectRowShowsRestaurantDetailScreen() {
-        var subject: RestaurantListViewController!
-        var router: FakeRouter!
-        var repo: FakeRestaurantRepo!
-
-        UIView.setAnimationsEnabled(false)
-        router = FakeRouter()
-        repo = FakeRestaurantRepo()
-
-        repo.allRestaurants = [
-            Restaurant(id: 1, name: "つけめんTETSU", address: "", cuisineType: "つけめん", offersEnglishMenu: true, walkInsOk: true, acceptsCreditCards: true, notes: "This place is great", author: "Simon", createdAt: NSDate(timeIntervalSince1970: 1454480320), photoUrl: NSURL(string: "")),
-            Restaurant(id: 2, name: "とんかつ 豚組食堂"),
-            Restaurant(id: 3, name: "Coco Curry"),
-        ]
-        subject = RestaurantListViewController(router: router, repo: repo)
-        subject.view.layoutSubviews()
-
-        subject.didTapRestaurant(1)
-        expect(router.restaurantDetailScreenIsShowing).to(equal(true))
+        restaurantListVC.didTapRestaurant(1)
+        expect(self.router.restaurantDetailScreenIsShowing).to(equal(true))
     }
 
 }
