32be5e9c4b06fb74b191f0144100f1ec1ff7d4a3
Derek Boire <dboire@pivotal.io>
2016-02-16 10:33:48 +0900

    Remove unused method in fake router
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- OsusumeTests/Fakes/FakeRouter.swift
+++ OsusumeTests/Fakes/FakeRouter.swift
@@ -3,31 +3,26 @@
 class FakeRouter : Router {
     var newRestaurantScreenIsShowing = false
     func showNewRestaurantScreen() {
-        setAllToFalse()
         newRestaurantScreenIsShowing = true
     }
 
     var restaurantListScreenIsShowing = false
     func showRestaurantListScreen() {
-        setAllToFalse()
         restaurantListScreenIsShowing = true
     }
 
     var restaurantDetailScreenIsShowing = false
     func showRestaurantDetailScreen(id: Int) {
-        setAllToFalse()
         restaurantDetailScreenIsShowing = true
     }
 
     var editRestaurantScreenIsShowing = false
     func showEditRestaurantScreen(restaurant: Restaurant) {
-        setAllToFalse()
         editRestaurantScreenIsShowing = true
     }
 
     var loginScreenIsShowing = false
     func showLoginScreen() {
-        setAllToFalse()
         loginScreenIsShowing = true
     }
 
@@ -36,14 +31,5 @@ class FakeRouter : Router {
     func showNewCommentScreen(id: Int) {
         newCommentScreenIsShowing = true
         showNewCommentScreen_args = id
-    }
-
-
-    func setAllToFalse() {
-        newRestaurantScreenIsShowing = false
-        restaurantListScreenIsShowing = false
-        restaurantDetailScreenIsShowing = false
-        editRestaurantScreenIsShowing = false
-        loginScreenIsShowing = false
     }
 }
