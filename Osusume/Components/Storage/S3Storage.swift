68d88920056f54589deae818991d7aa140ce1c51
Eno Compton <ecompton@pivotal.io>
2016-03-01 08:53:36 +0900

    Simplify S3 configuration

--- Osusume/Components/Storage/S3Storage.swift
+++ Osusume/Components/Storage/S3Storage.swift
@@ -5,16 +5,25 @@ protocol RemoteStorage {
 struct S3Storage: RemoteStorage {
 
     func uploadFile(withUrl url: NSURL) -> String {
+        let credentialsProvider = AWSCognitoCredentialsProvider(
+            regionType: AWSRegionType.APNortheast1,
+            identityPoolId: "ap-northeast-1:f65d8e6f-ac25-46b1-8106-f72026005681"
+        )
 
-        configureCredentials()
+        let configuration = AWSServiceConfiguration(
+            region: AWSRegionType.APNortheast1,
+            credentialsProvider: credentialsProvider
+        )
+
+        let serviceManager = AWSServiceManager.defaultServiceManager()
+        serviceManager.defaultServiceConfiguration = configuration
 
         let key = url.lastPathComponent!
         let bucketName = "osusume-tokyo-dev"
-
         let s3 = AWSS3TransferManager.defaultS3TransferManager()
-        let uploadRequest = AWSS3TransferManagerUploadRequest()
 
-        uploadRequest.bucket = bucketName
+        let uploadRequest = AWSS3TransferManagerUploadRequest()
+        uploadRequest.bucket = "osusume-tokyo-dev"
         uploadRequest.key = key
         uploadRequest.body = url
         uploadRequest.ACL = AWSS3ObjectCannedACL.PublicRead
@@ -23,24 +32,4 @@ struct S3Storage: RemoteStorage {
 
         return "https://s3-ap-northeast-1.amazonaws.com/\(bucketName)/\(key)"
     }
-
-    private func configureCredentials() {
-        let identityPoolId = "ap-northeast-1:f65d8e6f-ac25-46b1-8106-f72026005681"
-        let regionType = AWSRegionType.APNortheast1
-
-        let credentialsProvider = AWSCognitoCredentialsProvider(
-            regionType: regionType,
-            identityPoolId: identityPoolId
-        )
-
-        let configuration = AWSServiceConfiguration(
-            region: regionType,
-            credentialsProvider: credentialsProvider
-        )
-
-        AWSServiceManager
-            .defaultServiceManager()
-            .defaultServiceConfiguration = configuration
-    }
-
 }
