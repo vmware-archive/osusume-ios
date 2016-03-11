8e5d1e2e1ac0f2d7fb7f6d2779bbfde563ab88d9
Robert Gravina <rgravina@pivotal.io>
2016-03-11 11:38:09 +0900

    Explicitly layout view in test.
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- OsusumeTests/Screens/Profile/ProfileViewControllerTest.swift
+++ OsusumeTests/Screens/Profile/ProfileViewControllerTest.swift
@@ -18,28 +18,35 @@ class ProfileViewControllerTest: XCTestCase {
             userRepo: fakeUserRepo,
             sessionRepo: fakeSessionRepo
         )
-        profileVC.view.setNeedsLayout()
     }
 
     // MARK: View Lifecycle
     func test_viewDidLoad_displaysUsername() {
+        profileVC.view.setNeedsLayout()
+
         fakeUserRepo.stringPromise.success("A")
         NSRunLoop.osu_advance()
         expect(self.profileVC.userNameLabel.text).to(equal("A"))
     }
 
     func test_viewDidLoad_showsLogoutButton() {
+        profileVC.view.setNeedsLayout()
+
         expect(self.profileVC.logoutButton).toNot(beNil())
         expect(self.profileVC.logoutButton.titleLabel?.text).to(equal("Logout"))
     }
 
     func test_viewDidLoad_showsMyPostsTab() {
+        profileVC.view.setNeedsLayout()
+
         expect(self.profileVC.restaurantsLabel).toNot(beNil())
         expect(self.profileVC.restaurantsLabel.text).to(equal("My Posts"))
     }
 
     //MARK: Actions
     func test_tapLogout_logsOutUser() {
+        profileVC.view.setNeedsLayout()
+
         profileVC.logoutButton.sendActionsForControlEvents(.TouchUpInside)
         expect(self.fakeSessionRepo.deleteTokenWasCalled).to(beTrue())
         expect(self.fakeRouter.loginScreenIsShowing).to(beTrue())
