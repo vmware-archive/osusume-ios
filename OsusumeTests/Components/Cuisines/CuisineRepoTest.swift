aede0f6eb75fe86571987a25b479f40c5d9d50ac
Kyle Gearhart <kgearhart@pivotal.io>
2016-03-23 17:07:18 +0900

    Clean-up CuisineRepoTest
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- OsusumeTests/Components/Cuisines/CuisineRepoTest.swift
+++ OsusumeTests/Components/Cuisines/CuisineRepoTest.swift
@@ -5,6 +5,8 @@ import Nimble
 @testable import Osusume
 
 class FakeCuisineListParser: DataListParser {
+    typealias ParsedObject = [Cuisine]
+
     var parse_arg: [[String : AnyObject]]!
     var parse_returnValue = Result<[Cuisine], ParseError>(value: [])
     func parse(json: [[String : AnyObject]]) -> Result<[Cuisine], ParseError> {
@@ -14,62 +16,37 @@ class FakeCuisineListParser: DataListParser {
 }
 
 class CuisineRepoTest: XCTestCase {
+    let fakeHttp = FakeHttp()
+    let fakeCuisineListParser = FakeCuisineListParser()
+    let cuisineJsonPromise = Promise<AnyObject, RepoError>()
+    var cuisineRepo: HttpCuisineRepo<FakeCuisineListParser>!
+
+    override func setUp() {
+        fakeHttp.get_returnValue = cuisineJsonPromise.future
+
+        cuisineRepo = HttpCuisineRepo(
+            http: self.fakeHttp,
+            parser: self.fakeCuisineListParser
+        )
+    }
 
     func test_getAll_makesGetRequestWithCorrectArguments() {
-        let fakeHttp = FakeHttp()
-        let fakeCuisineListParser = FakeCuisineListParser()
-
-        let cuisineRepo = HttpCuisineRepo(
-            http: fakeHttp,
-            parser: fakeCuisineListParser
-        )
-
-
         cuisineRepo.getAll()
 
-
-        expect(fakeHttp.get_args.path).to(equal("/cuisines"))
+        expect(self.fakeHttp.get_args.path).to(equal("/cuisines"))
     }
 
     func test_getAll_parsesHttpOutputJson() {
-        let fakeHttp = FakeHttp()
-        let fakeSessionRepo = FakeSessionRepo()
-        let fakeCuisineListParser = FakeCuisineListParser()
-        fakeSessionRepo.tokenValue = "A Validated Token Value"
-
-        let cuisineJsonPromise = Promise<AnyObject, RepoError>()
-        fakeHttp.get_returnValue = cuisineJsonPromise.future
-
-        let cuisineRepo = HttpCuisineRepo(
-            http: fakeHttp,
-            parser: fakeCuisineListParser
-        )
-
-
         cuisineRepo.getAll()
         cuisineJsonPromise.success([["Cuisine" : "Json"]])
 
 
         NSRunLoop.osu_advance()
 
-        expect(fakeCuisineListParser.parse_arg).to(equal([["Cuisine" : "Json"]]))
+        expect(self.fakeCuisineListParser.parse_arg).to(equal([["Cuisine" : "Json"]]))
     }
 
     func test_getAll_returnsParsedCuisineListResult() {
-        let fakeHttp = FakeHttp()
-        let fakeSessionRepo = FakeSessionRepo()
-        let fakeCuisineListParser = FakeCuisineListParser()
-        fakeSessionRepo.tokenValue = "A Validated Token Value"
-
-        let cuisineJsonPromise = Promise<AnyObject, RepoError>()
-        fakeHttp.get_returnValue = cuisineJsonPromise.future
-
-        let cuisineRepo = HttpCuisineRepo(
-            http: fakeHttp,
-            parser: fakeCuisineListParser
-        )
-
-
         let getAllCuisinesFuture = cuisineRepo.getAll()
         fakeCuisineListParser.parse_returnValue = Result.Success(
             [Cuisine(id: 1, name: "Thai")]
