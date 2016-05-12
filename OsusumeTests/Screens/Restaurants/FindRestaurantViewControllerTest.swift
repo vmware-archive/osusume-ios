b342b432cd363f1b98eb77027bcaf2ca2d217b5d
Derek Boire <dboire@pivotal.io>
2016-05-12 17:54:39 +0900

    Refactor test code to use setup function.
    Signed-off-by: Kyle Gearhart <kgearhart@pivotal.io>

--- OsusumeTests/Screens/Restaurants/FindRestaurantViewControllerTest.swift
+++ OsusumeTests/Screens/Restaurants/FindRestaurantViewControllerTest.swift
@@ -3,35 +3,35 @@ import Nimble
 @testable import Osusume
 
 class FindRestaurantViewControllerTest: XCTestCase {
+    var findRestaurantViewController: FindRestaurantViewController!
+
+    override func setUp() {
+        findRestaurantViewController = FindRestaurantViewController()
+        findRestaurantViewController.view.setNeedsLayout()
+    }
 
     func test_viewDidLoad_initializesSubviews() {
-        let findRestaurantViewController = FindRestaurantViewController()
-        findRestaurantViewController.view.setNeedsLayout()
-
-
-        expect(findRestaurantViewController.restaurantNameTextField).to(beAKindOf(UITextField))
+        expect(self.findRestaurantViewController.restaurantNameTextField)
+            .to(beAKindOf(UITextField))
     }
 
     func test_viewDidLoad_addsSubviews() {
-        let findRestaurantViewController = FindRestaurantViewController()
-        findRestaurantViewController.view.setNeedsLayout()
-
-
-        expect(findRestaurantViewController.view).to(containSubview(findRestaurantViewController.restaurantNameTextField))
+        expect(self.findRestaurantViewController.view)
+            .to(containSubview(findRestaurantViewController.restaurantNameTextField))
     }
 
     func test_viewDidAppear_setsRestaurantNameTextFieldAsFirstResponder() {
-        let findRestaurantViewController = FindRestaurantViewController()
-
         var window: UIWindow?
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window!.rootViewController = findRestaurantViewController
         window!.makeKeyAndVisible()
 
+
         findRestaurantViewController.view.setNeedsLayout()
         NSRunLoop.osu_advance()
 
 
-        expect(findRestaurantViewController.restaurantNameTextField.isFirstResponder()).to(beTrue())
+        expect(self.findRestaurantViewController.restaurantNameTextField.isFirstResponder())
+            .to(beTrue())
     }
 }
