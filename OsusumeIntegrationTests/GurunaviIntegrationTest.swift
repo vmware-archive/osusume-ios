b9015268707cf86cc7946890eb76c598fb5b3768
Chiharu Miya <cmiya@pivotal.io>
2016-05-16 15:42:36 +0900

    Add Gurunavi integration test for search result.
    Signed-off-by: Derek Boire <dboire@pivotal.io>

---
+++ OsusumeIntegrationTests/GurunaviIntegrationTest.swift
@@ -0,0 +1,25 @@
+import XCTest
+import BrightFutures
+import Nimble
+@testable import Osusume
+
+class GurunaviIntegrationTest: XCTestCase {
+
+    func test_query_returnsSearchResults() {
+        let gnaviRestaurantSearchRepo: GNaviRestaurantSearchRepo = GNaviRestaurantSearchRepo(
+            http: DefaultHttp(basePath: ""),
+            parser: GNaviRestaurantListParser()
+        )
+
+
+        let result: Future<[SearchResultRestaurant], RepoError> = gnaviRestaurantSearchRepo.getForSearchTerm("すしざんまい")
+        NSRunLoop.osu_advance(by: 5)
+
+
+        let restaurant: SearchResultRestaurant? = result.value?.first
+        expect(restaurant?.name).to(contain("すしざんまい"))
+        expect(restaurant?.id).toNot(beNil())
+        expect(restaurant?.address).toNot(beNil())
+    }
+
+}
